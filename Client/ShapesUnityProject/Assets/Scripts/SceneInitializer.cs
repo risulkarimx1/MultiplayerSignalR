74ced40a55fe8c4e063041827020f7a9f308ccd4
Md Risul Karim <risulkarimx1@gmail.com>
2021-02-12 20:52:13 +0100

    added unirx to client

---
+++ Client/ShapesUnityProject/Assets/Scripts/SceneInitializer.cs
@@ -0,0 +1,35 @@
+﻿using SigClient4;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SceneInitializer : MonoBehaviour
+    {
+        private SignalRClientContext _clientContext;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _clientContext = new SignalRClientContext();
+            _clientContext.ReceivedMessage += OnMessageReceived;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                _clientContext.SendMessageAsync("risul", "from unity");
+            }
+        }
+
+        private void OnMessageReceived(object sender, MessageArgs e)
+        {
+            Debug.Log($"{e.Name} - {e.Message}");
+        }
+
+        private void OnDestroy()
+        {
+            _clientContext.ReceivedMessage -= OnMessageReceived;
+            _clientContext.Dispose();
+        }
+    }
+}
---
+++ Client/ShapesUnityProject/Assets/Scripts/SignalRClientContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using SigClient4;
+
+namespace Assets.Scripts
+{
+    public class SignalRClientContext : IDisposable
+    {
+        public EventHandler<MessageArgs> ReceivedMessage = delegate { };
+        private HubConnection _hubConnection;
+        private IHubProxy _chatHub;
+        private IDisposable _messageProxy;
+
+        public SignalRClientContext()
+        {
+            _hubConnection = new HubConnection("http://localhost:8080/chatHub");
+            _chatHub = _hubConnection.CreateHubProxy("ChatHub");
+            _messageProxy = _chatHub.On<string, string>("SendMessageToClient", MessageFromServer);
+            _hubConnection.Start().ConfigureAwait(false);
+        }
+
+        public Task SendMessageAsync(string name, string message)
+        {
+            return _chatHub.Invoke("SendMessage", new object[] {name, message});
+        }
+
+        private void MessageFromServer(string name, string message)
+        {
+            ReceivedMessage.Invoke(this, new MessageArgs() {Message = message, Name = name});
+        }
+
+        public void Dispose()
+        {
+            _hubConnection.Dispose();
+            _messageProxy.Dispose();
+        }
+    }
+}
